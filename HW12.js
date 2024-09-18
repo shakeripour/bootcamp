3c280c4ce54398c18607f979e2ea6324b72fc4e1
Ehsan Shakeripour <ehsan.shakeripour@gmail.com>
2024-09-18 15:49:22 +0330

    hw12 template

---
+++ HW12.js
@@ -0,0 +1,23 @@
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+    this.isCheckedOut = false;
+  }
+
+  // Add checkOut() method here
+
+  // Add returnBook() method here
+}
+
+class Library {
+  constructor() {
+    this.books = [];
+  }
+
+  // Add addBook(book) method here
+
+  // Add displayAvailableBooks() method here
+}
+
+// Test your implementation here
