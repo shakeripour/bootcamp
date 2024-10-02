0c52264b1d9c3b865a023576c5a231ae70b17c4c
Ehsan Shakeripour <ehsan.shakeripour@gmail.com>
2024-10-02 19:10:43 +0330

    session 15

---
+++ Componenets/utils.js
@@ -0,0 +1,5 @@
+const generateFullName = (firstname, lastName) => {
+    return `My fullname is ${firstname} ${lastName}`;
+};
+
+module.exports = generateFullName;
---
+++ sample.js
@@ -0,0 +1,24 @@
+// import getFullName from './Componenets/utils.js';
+// import getFullName from './Componenets/utils.js';
+// import getFullName from './Componenets/utils.js';
+// import getFullName from './Componenets/utils.js';
+// import getFullName from './Componenets/utils.js';
+
+// import { export1, export2 as alias2, /* … */ } from "module-name";
+
+// const getFullName = require('./Componenets/utils.js');
+
+// console.log(getFullName('Ehsan','Shakeripour'));
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+
+
+
+
+
